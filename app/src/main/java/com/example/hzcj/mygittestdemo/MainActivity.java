0b450f1c8454cb4af28b4f08d5ecbe89a3f3efda
zhu <18911664778@163.com>
2017-08-18 17:19:36 +0800

    功能3提交

--- app/src/main/java/com/example/hzcj/mygittestdemo/MainActivity.java
+++ app/src/main/java/com/example/hzcj/mygittestdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hzcj.mygittestdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+
+    }
+    /**
+     * feature-1
+     * display HelloWorld
+     * @param view
+     */
+    public void feature1(View view) {
+        Log.d("tag","功能1");
+    }
+
+    /**
+     * feature-2
+     * display HelloWorld
+     * @param view
+     */
+    public void feature2(View view) {
+        Log.d("tag","功能2");
+    }
+    /**
+     * feature-3
+     * display HelloWorld
+     * @param view
+     */
+    public void feature3(View view) {
+        Log.d("tag","功能3");
     }
 }
